aa9d1a19e6a6a84904be6653771729d3d617312a
Kevin Xu <kevinxu8099@gmail.com>
2020-10-17 17:20:48 -0700

    Add NLP

--- src/main/java/SwishyListener.java
+++ src/main/java/SwishyListener.java
@@ -2,6 +2,7 @@ import com.gikk.twirk.events.TwirkListener;
 import com.gikk.twirk.types.twitchMessage.TwitchMessage;
 import com.gikk.twirk.types.users.TwitchUser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +13,15 @@ public class SwishyListener implements TwirkListener {
 
 
     public void onPrivMsg(TwitchUser sender, TwitchMessage message){
+        List<UserMessageEvent> curUserMessages;
+
+        curUserMessages  = allMessages.getOrDefault(sender.getUserID(), new ArrayList<>());
+
+        curUserMessages.add(new UserMessageEvent(sender.getDisplayName(), 0.00, message.getContent()));
+
+
+        allMessages.put(sender.getUserID(), curUserMessages);
+
 
     }
 
